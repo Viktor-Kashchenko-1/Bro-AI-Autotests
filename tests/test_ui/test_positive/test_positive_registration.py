290f4e6929c29c8e649db3204d5e28c4d3c6830d
Viktor.Kashchcenko <lordpeka9@gmail.com>
2025-04-15 19:01:19 +0300

    v 0.1.9 Refactor UI tests after fix Bro-AI website stand. Add some new API user_registration tests, minor changes existing tests and configs.

--- tests/test_ui/test_positive/test_positive_registration.py
+++ tests/test_ui/test_positive/test_positive_registration.py
@@ -63,11 +63,11 @@ def test_registration_min_and_max_email(browser, faker_data, base_url_ui, wait, 
     browser.find_element(By.ID, 'email').send_keys(random_min_and_max_email)
 
     browser.find_element(By.CSS_SELECTOR, '.ui.button.blue').click()
-    wait.until(EC.url_to_be(f'{base_url_ui}/sign_up'))  # как поправят будет /логин
+    wait.until(EC.url_to_be(f'{base_url_ui}/login'))
 
     alert = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"[role='alert'] div:last-child")))
     text = alert.get_attribute('textContent')
-    assert text == 'Что-то пошло не так. Пожалуйста, попробуйте позже'#|success_alert_message
+    assert text == success_alert_message
 
 
 #4 test
@@ -79,8 +79,7 @@ def test_registration_min_and_max_email(browser, faker_data, base_url_ui, wait, 
 #     ('test_sadlik24@mail.com', "Sadlik110", "Qqqqwerty123"),
 #     ('test_sadlik222@mail.com', "Sadlk123", "Qqwert123")
 #])
-def test_positive_registration_minimum_length_passwords(browser, base_url_ui, wait, faker_data,
-                                                           fail_alert_message, success_alert_message):
+def test_positive_registration_minimum_length_passwords(browser, base_url_ui, wait, faker_data, success_alert_message):
     browser.get(base_url_ui+'/sign_up')
     wait.until(EC.presence_of_element_located((By.ID, 'username'))).send_keys(faker_data['name'])
     browser.find_element(By.ID, 'pass1').send_keys(faker_data['password'])
@@ -88,10 +87,10 @@ def test_positive_registration_minimum_length_passwords(browser, base_url_ui, wa
     browser.find_element(By.ID, 'email').send_keys(faker_data['email'])
 
     browser.find_element(By.CSS_SELECTOR, '.ui.button.blue').click()
-    wait.until(EC.url_to_be(f'{base_url_ui}/sign_up')) # как поправят будет /логин
+    wait.until(EC.url_to_be(f'{base_url_ui}/login'))
 
     alert = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[role='alert'] div:last-child")))
-    assert alert.get_attribute('textContent') == fail_alert_message  #времянка вместо 'Пользователь с таким email уже зарегистрирован'
+    assert alert.get_attribute('textContent') == success_alert_message
 
 
 #5 test
@@ -108,7 +107,7 @@ def test_positive_registration_minimum_length_passwords(browser, base_url_ui, wa
     lambda: fake_element.password(length=50, digits=True , upper_case=False, special_chars=False, lower_case= False)
 ])
 def test_positive_registration_maximum_length_passwords(browser, base_url_ui, password, wait, faker_data,
-                                                           fail_alert_message, success_alert_message):
+                                                        success_alert_message):
     browser.get(base_url_ui+'/sign_up')
     wait.until(EC.presence_of_element_located((By.ID, 'username'))).send_keys(faker_data['name'])
     current_password = password()
@@ -117,17 +116,17 @@ def test_positive_registration_maximum_length_passwords(browser, base_url_ui, pa
     browser.find_element(By.ID, 'email').send_keys(faker_data['email'])
 
     browser.find_element(By.CSS_SELECTOR, '.ui.button.blue').click()
-    wait.until(EC.url_to_be(f'{base_url_ui}/sign_up')) # как поправят будет /логин
+    wait.until(EC.url_to_be(f'{base_url_ui}/login'))
 
     alert = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[role='alert'] div:last-child")))
-    assert alert.get_attribute('textContent') == fail_alert_message #|success_alert_message
+    assert alert.get_attribute('textContent') == success_alert_message
+
 
 #6 test
 @pytest.mark.ui
 @pytest.mark.positive
 @pytest.mark.registration_positive
-def test_positive_registration_minimum_length_username(browser, base_url_ui, wait, faker_data,
-                                                       fail_alert_message, success_alert_message):
+def test_positive_registration_minimum_length_username(browser, base_url_ui, wait, faker_data, success_alert_message):
     username = 'a'
     browser.get(base_url_ui+'/sign_up')
     wait.until(EC.presence_of_element_located((By.ID, 'username'))).send_keys(username)
@@ -136,17 +135,17 @@ def test_positive_registration_minimum_length_username(browser, base_url_ui, wai
     browser.find_element(By.ID, 'email').send_keys(faker_data['email'])
 
     browser.find_element(By.CSS_SELECTOR, '.ui.button.blue').click()
-    wait.until(EC.url_to_be(f'{base_url_ui}/sign_up')) # как поправят будет /логин
+    wait.until(EC.url_to_be(f'{base_url_ui}/login'))
 
     alert = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[role='alert'] div:last-child")))
-    assert alert.get_attribute('textContent') == fail_alert_message #|success_alert_message
+    assert alert.get_attribute('textContent') == success_alert_message
+
 
 #7 test
 @pytest.mark.ui
 @pytest.mark.positive
 @pytest.mark.registration_positive
-def test_positive_registration_maximum_length_username(browser, base_url_ui, wait, faker_data,
-                                                           fail_alert_message, success_alert_message):
+def test_positive_registration_maximum_length_username(browser, base_url_ui, wait, faker_data, success_alert_message):
     username = 'A' * 50
     browser.get(base_url_ui + '/sign_up')
     wait.until(EC.presence_of_element_located((By.ID, 'username'))).send_keys(username)
@@ -155,18 +154,18 @@ def test_positive_registration_maximum_length_username(browser, base_url_ui, wai
     browser.find_element(By.ID, 'email').send_keys(faker_data['email'])
 
     browser.find_element(By.CSS_SELECTOR, '.ui.button.blue').click()
-    wait.until(EC.url_to_be(f'{base_url_ui}/sign_up')) # как поправят будет /логин
+    wait.until(EC.url_to_be(f'{base_url_ui}/login'))
 
     alert = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[role='alert'] div:last-child")))
-    assert alert.get_attribute('textContent') == fail_alert_message #|success_alert_message
+    assert alert.get_attribute('textContent') == success_alert_message
+
 
 #8 test (20 iteration) check valid email domains for the USA, UA, RU countries
 @pytest.mark.ui
 @pytest.mark.positive
 @pytest.mark.registration_positive
 @pytest.mark.multi_core_fail
-def test_valid_email_domains(browser, base_url_ui, faker_data, wait, e_domain,
-                             fail_alert_message, success_alert_message):
+def test_valid_email_domains(browser, base_url_ui, faker_data, wait, e_domain, success_alert_message):
     browser.get(f'{base_url_ui}/sign_up')
     username_field = wait.until(EC.presence_of_element_located((By.ID, 'username')))
     username_field.send_keys(faker_data['name'])
@@ -174,10 +173,10 @@ def test_valid_email_domains(browser, base_url_ui, faker_data, wait, e_domain,
     browser.find_element(By.ID, 'pass2').send_keys(faker_data['password'])
     browser.find_element(By.ID, "email").send_keys(f'{faker_data['name']}@{e_domain[1]}')
     browser.find_element(By.CSS_SELECTOR, '.ui.button.blue').click()
-    #wait.until(EC.url_to_be(f'{base_url_ui}/login')) Тo do распечатать когда заработает
+    wait.until(EC.url_to_be(f'{base_url_ui}/login'))
     alert = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[role='alert'] div:last-child")))
 
-    assert alert.get_attribute('textContent') == fail_alert_message # времянка вместо 'Вы успешно зарегистрировались'
+    assert alert.get_attribute('textContent') == 'Вы успешно зарегистрировались'
 
 
 # @pytest.mark.ui
